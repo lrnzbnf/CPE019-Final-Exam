2218cda094c35e4b8f00f3d2fa16c0dde10d12e8
lrnzbnf <158253790+lrnzbnf@users.noreply.github.com>
2024-05-16 22:00:55 +0800

    Update computer_components_classifier_app.py

--- computer_components_classifier_app.py
+++ computer_components_classifier_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1LMTkC7m3fUzFCsVFvwVFi5dqvLTuz62J
 """
 
-!pip install streamlit
 
 import streamlit as st
 import tensorflow as tf
